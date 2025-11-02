0ede6c3a7b1b3dedc961dd1e9fd27a32e4761672
PavelZur <128070361+PavelZur@users.noreply.github.com>
2025-11-02 16:50:19 +0300

    правки по прицелу, камере, стрельба через стены, собрал уровень

--- Assets/_Game/Scripts/Weapon/RayVisualizer.cs
+++ Assets/_Game/Scripts/Weapon/RayVisualizer.cs
@@ -2,23 +2,27 @@ using UnityEngine;
 
 public class RayVisualizer : MonoBehaviour
 {
-    [SerializeField] private Transform _bulletStartTransform;
+    [SerializeField] private WeaponController _weaponController;
     [SerializeField] private LineRenderer _lineRenderer;
     [SerializeField] private GameObject hitSpherePrefab;
 
+
     void LateUpdate()
     {
-        Ray ray = new Ray(_bulletStartTransform.position, _bulletStartTransform.forward);
-        RaycastHit hit;
-        
-        if (Physics.Raycast(ray, out hit, 100f))
+        if (_weaponController.CurrentActiveWeapon != null)
         {
-            DrawLine(_bulletStartTransform.position, hit.point);
-        }
-        else
-        {
-            DrawLine(_bulletStartTransform.position, _bulletStartTransform.position + (ray.direction * 100f));
-        }
+            Ray ray = new Ray(_weaponController.CurrentActiveWeapon.BulletStartTranform.position, _weaponController.CurrentActiveWeapon.BulletStartTranform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                DrawLine(_weaponController.CurrentActiveWeapon.BulletStartTranform.position, hit.point);
+            }
+            else
+            {
+                DrawLine(_weaponController.CurrentActiveWeapon.BulletStartTranform.position, _weaponController.CurrentActiveWeapon.BulletStartTranform.position + (ray.direction * 100f));
+            }
+        }   
     }
 
     private void DrawLine(Vector3 StartPos, Vector3 endPos)
